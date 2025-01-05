a87cc79a856a23c9dd88ef3491efd6e98c502f48
sphericle <86196223+sphericle@users.noreply.github.com>
2025-01-05 10:15:03 -0500

    cool

--- src/views/MusicOpinions.vue
+++ src/views/MusicOpinions.vue
@@ -3,17 +3,27 @@
     <div class="grid-container">
       <div class="grid-item song">
         <h1>Favorite Song</h1>
+        <img src="https://miro.medium.com/v2/resize:fit:1400/1*5_pwedLFyMKMI4GaA7w8zQ.jpeg">
+        <p>Frank Ocean - Nights</p>
       </div>
       <div class="grid-item artist">
         <h1>Favorite Artist</h1>
+        <img src="https://i.scdn.co/image/ab6761610000e5ebdfa2b0c7544a772042a12e52">
+        <p>Tyler, The Creator</p>
       </div>
       <div class="grid-item album">
         <h1>Favorite Album</h1>
+        <img src="https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/fd/fd/8c/fdfd8c26-b8f9-4768-41d3-b24773250c65/886446605814.jpg/1200x1200bb.jpg">
+        <p>Tyler, The Creator - Flower Boy</p>
       </div>
       <div class="grid-item playlist">
         <h1>My Playlist</h1>
+        <img src="https://miro.medium.com/v2/resize:fit:1400/1*5_pwedLFyMKMI4GaA7w8zQ.jpeg">
+        <p>sldkfjsdflkjsdf</p>
       </div>
     </div>
+
+    <p class="message">i'm not gay</p>
 
     
 
@@ -32,7 +42,18 @@
     text-align: center;
   }
   .grid-item {
-    padding: 20px;
-    width: 0.5fr;
+    padding: 40px;
+  }
+
+  img {
+    height: 2in
+  }
+
+
+  .message {
+    padding: 1in;
+    align-items: center;
+    justify-items: center;
+    text-align: center;
   }
 </style>
